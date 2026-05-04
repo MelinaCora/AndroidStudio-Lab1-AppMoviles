94df8b8ded89bf254cdeee64dfcb39909a3e9c34
MelinaCora <melinaccora97@gmail.com>
2026-05-04 17:15:54 -0300

    Pantalla rotativa y flecha volver atras

--- app/src/main/java/com/example/lab1_appmoviles/DetalleActivity.kt
+++ app/src/main/java/com/example/lab1_appmoviles/DetalleActivity.kt
@@ -4,36 +4,44 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.widget.AppCompatButton
 
 class DetalleActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NOMBRE = "NOMBRE_USUARIO"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
 
+        // Toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         supportActionBar?.title = "Detalle"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val texto = findViewById<TextView>(R.id.textSaludo)
-        val inicial = findViewById<TextView>(R.id.textInicial)
 
-        val nombre = intent.getStringExtra("NOMBRE_USUARIO") ?: "?"
+        val textoSaludo = findViewById<TextView>(R.id.textSaludo)
+        val textoInicial = findViewById<TextView>(R.id.textInicial)
 
-        // Mostrar saludo
-        texto.text = "Hola, $nombre!"
+        val nombre = intent.getStringExtra(EXTRA_NOMBRE) ?: "?"
 
-        //primera letra
-        val letra = nombre.first().uppercase()
+        textoSaludo.text = "Hola, $nombre!"
+        textoInicial.text = nombre.first().uppercase()
 
-        // Mostrar inicial
-        inicial.text = letra
-
-        // Botón volver
-        val botonVolver = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.buttonVolver)
+        //boton volver
+        val botonVolver = findViewById<AppCompatButton>(R.id.buttonVolver)
         botonVolver.setOnClickListener {
             finish()
         }
+    }
+
+    // Flecha
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
